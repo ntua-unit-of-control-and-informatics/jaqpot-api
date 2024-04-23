63b3bd7ba72eef7269c2f5b28f83a7cf4d264765
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-04-23 17:04:06 +0300

    feat: initial commit

---
+++ src/main/kotlin/org/jaqpot/jaqpotapiv2/JaqpotApiV2Application.kt
@@ -0,0 +1,11 @@
+package org.jaqpot.jaqpotapiv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JaqpotApiV2Application
+
+fun main(args: Array<String>) {
+    runApplication<JaqpotApiV2Application>(*args)
+}
---
+++ src/test/kotlin/org/jaqpot/jaqpotapiv2/JaqpotApiV2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jaqpot.jaqpotapiv2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JaqpotApiV2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
