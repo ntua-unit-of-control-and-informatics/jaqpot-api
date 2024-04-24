5600e42b6e3953ceef30f6347c7886f8e98c9651
Alex Arvanitidis <alex.arvanitidis@mimacom.com>
2024-04-24 17:02:06 +0300

    feat: add docker compose for keycloak and postgres (#1)

---
+++ src/main/kotlin/org/jaqpot/jaqpotapiv2/controller/OAuth2ResourceServerController.kt
@@ -0,0 +1,20 @@
+package org.jaqpot.jaqpotapiv2.controller
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OAuth2ResourceServerController {
+
+    @GetMapping("/")
+    fun index(@AuthenticationPrincipal jwt: Jwt): String {
+        return String.format("Hello, %s!", jwt.subject)
+    }
+
+    @GetMapping("/message")
+    fun message(): String {
+        return "secret message"
+    }
+}
