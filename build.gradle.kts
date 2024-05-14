f430ab431c8b771c25791d1db534557ddbef25d9
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-05-14 13:07:09 +0300

    feat(JAQPOT-1): add openapi generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     id("org.jetbrains.kotlin.jvm") version "1.9.23"
     id("org.jetbrains.kotlin.plugin.spring") version "1.9.23"
+
+    id("org.openapi.generator") version "7.5.0"
 }
 
 group = "org.jaqpot"
@@ -29,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.21")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
@@ -40,7 +43,42 @@ dependencies {
     testImplementation("org.springframework.security:spring-security-test")
 }
 
+tasks.compileKotlin {
+    dependsOn(tasks.openApiGenerate)
+}
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$projectDir/src/main/resources/api-swagger.yaml")
+    outputDir.set("$buildDir/generated/openapi")
+    apiPackage.set("org.jaqpot.api.controller")
+    modelPackage.set("org.jaqpot.api.entity")
+    modelNamePrefix.set("Dto")
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8",
+            "hideGenerationTimestamp" to "true",
+            "interfaceOnly" to "true",
+            "library" to "spring-boot",
+            "serializableModel" to "true",
+            "useBeanValidation" to "true",
+            "useTags" to "true",
+            "implicitHeaders" to "true",
+            "openApiNullable" to "false",
+            "delegatePattern" to "true",
+            "useSwaggerUI" to "true"
+        )
+    )
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDirs("$buildDir/generated/openapi/src/main/java")
+        }
+    }
 }
