a8d082bfc09ae46686e74e636ace1b88af5102b3
alarv <alarvfm@gmail.com>
2024-05-31 11:08:57 +0300

    fix: fix build

--- src/test/kotlin/org/jaqpot/api/JaqpotApiApplicationTests.kt
+++ src/test/kotlin/org/jaqpot/api/JaqpotApiApplicationTests.kt
@@ -1,10 +1,11 @@
 package org.jaqpot.api
 
+import org.jaqpot.api.integration.AbstractIntegrationTest
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class JaqpotApiApplicationTests {
+class JaqpotApiApplicationTests : AbstractIntegrationTest() {
 
     @Test
     fun contextLoads() {
