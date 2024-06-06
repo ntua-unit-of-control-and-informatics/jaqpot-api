e4544059103689689c6a452c35c233500da65f2d
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-06-06 14:47:57 +0300

    feat: add KeycloakJwtConverter for admin role

--- src/main/kotlin/org/jaqpot/api/config/SecurityConfig.kt
+++ src/main/kotlin/org/jaqpot/api/config/SecurityConfig.kt
@@ -1,28 +1,37 @@
 package org.jaqpot.api.config
 
+import jakarta.servlet.DispatcherType
+import org.jaqpot.api.service.authentication.keycloak.KeycloakJwtConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.DispatcherTypeRequestMatcher
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(private val keycloakJwtConverter: KeycloakJwtConverter) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             authorizeRequests {
+                // TODO see if these 2 lines are needed @see https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html#_all_dispatches_are_authorized
+                authorize(DispatcherTypeRequestMatcher(DispatcherType.FORWARD), permitAll)
+                authorize(DispatcherTypeRequestMatcher(DispatcherType.ERROR), permitAll)
                 // allow all swagger urls
                 arrayOf("/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").map { it ->
                     authorize(it, permitAll)
                 }
+                authorize("/v1/datasets/{datasetId:[\\d+]}", hasAuthority("USER"))
 
                 authorize(anyRequest, authenticated)
             }
             oauth2ResourceServer {
-                jwt { }
+                jwt {
+                    jwtAuthenticationConverter = keycloakJwtConverter
+                }
             }
             csrf { disable() }
         }
