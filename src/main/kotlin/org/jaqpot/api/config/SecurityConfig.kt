c28c824de8e46d4b23f966b1e16409c67691f868
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-06-06 10:25:42 +0300

    fix: update security config to kotlin dsl

--- src/main/kotlin/org/jaqpot/api/config/SecurityConfig.kt
+++ src/main/kotlin/org/jaqpot/api/config/SecurityConfig.kt
@@ -2,30 +2,31 @@ package org.jaqpot.api.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
     @Bean
-    @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests { a ->
-            a
-                // allow swagger for everyone
-                .requestMatchers(
-                    "/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**"
-                )
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-        }
-            .oauth2ResourceServer { oauth2: OAuth2ResourceServerConfigurer<HttpSecurity?> ->
-                oauth2.jwt(Customizer.withDefaults())
+        http {
+            authorizeRequests {
+                // allow all swagger urls
+                arrayOf("/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").map { it ->
+                    authorize(it, permitAll)
+                }
+
+                authorize(anyRequest, authenticated)
             }
-            .csrf { csrf -> csrf.disable() }
+            oauth2ResourceServer {
+                jwt { }
+            }
+            csrf { disable() }
+        }
         return http.build()
     }
+
 }
