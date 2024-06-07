eb72adca0dc1e4e7d4567b282df69e5c311bfa17
alarv <alarvfm@gmail.com>
2024-06-07 19:49:10 +0300

    feat: initial organization sql migration script

---
+++ src/main/resources/db/migration/V3__create_organizations.sql
@@ -0,0 +1,39 @@
+CREATE TABLE organization
+(
+    id            BIGSERIAL                   NOT NULL,
+    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    name          VARCHAR(255) UNIQUE         NOT NULL,
+    admin_user_id VARCHAR(255)                NOT NULL,
+    description   TEXT,
+    contact_email VARCHAR(255),
+    contact_phone VARCHAR(255),
+    website       VARCHAR(255),
+    address       VARCHAR(255),
+    CONSTRAINT pk_organization PRIMARY KEY (id)
+);
+
+CREATE TABLE organization_models
+(
+    model_id        BIGINT NOT NULL,
+    organization_id BIGINT NOT NULL,
+    CONSTRAINT pk_organization_models PRIMARY KEY (model_id, organization_id)
+);
+
+CREATE TABLE organization_users
+(
+    organization_id BIGINT       NOT NULL,
+    user_id         VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE organization
+    ADD CONSTRAINT uc_organization_name UNIQUE (name);
+
+ALTER TABLE organization_users
+    ADD CONSTRAINT fk_organization_users_on_organization FOREIGN KEY (organization_id) REFERENCES organization (id);
+
+ALTER TABLE organization_models
+    ADD CONSTRAINT fk_orgmod_on_model FOREIGN KEY (model_id) REFERENCES model (id);
+
+ALTER TABLE organization_models
+    ADD CONSTRAINT fk_orgmod_on_organization FOREIGN KEY (organization_id) REFERENCES organization (id);
