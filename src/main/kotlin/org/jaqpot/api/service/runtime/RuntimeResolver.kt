f42fa90fd9177cc1613f92f2680effe37bf3169c
alarv <alarvfm@gmail.com>
2024-06-19 16:30:41 +0300

    refactor: cleanup RuntimeResolver code

--- src/main/kotlin/org/jaqpot/api/service/runtime/RuntimeResolver.kt
+++ src/main/kotlin/org/jaqpot/api/service/runtime/RuntimeResolver.kt
@@ -5,6 +5,7 @@ import org.jaqpot.api.entity.ModelType
 import org.jaqpot.api.error.JaqpotRuntimeException
 import org.jaqpot.api.service.runtime.config.RuntimeProvider
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
 class RuntimeResolver(
@@ -12,44 +13,58 @@ class RuntimeResolver(
 ) {
 
     fun resolveRuntimeUrl(model: Model): String {
-        if (model.legacyPredictionService != null) {
-            if (model.legacyPredictionService.contains("jaqpot-r")) {
-                return if (model.legacyPredictionService.contains("predict.pbpk")) {
-                    "${runtimeProvider.jaqpotRUrl}/ocpu/library/GenericR/R/predict.pbpk/json"
-                } else if (model.legacyPredictionService.contains("jaqpot.predict.caret")) {
-                    "${runtimeProvider.jaqpotRUrl}/ocpu/library/GenericR/R/jaqpot.predict.caret/json"
-                } else {
-                    throw JaqpotRuntimeException("unknown runtime with predictionService ${model.legacyPredictionService}")
+        val legacyResolveRuntime = legacyResolveRuntime(model)
+
+        return legacyResolveRuntime.orElse(
+            when (model.type) {
+                ModelType.R -> {
+                    runtimeProvider.jaqpotRUrl
                 }
-            } else if (model.legacyPredictionService.contains("jaqpot-inference.jaqpot")) {
-                return runtimeProvider.jaqpotpyInferenceV6Url
-            } else if (model.legacyPredictionService.contains("jaqpot-python")) {
-                return if (model.legacyPredictionService.contains("22")) {
-                    runtimeProvider.jaqpotpyInferenceLegacy22
-                } else if (model.legacyPredictionService.contains("23")) {
-                    runtimeProvider.jaqpotpyInferenceLegacy23
-                } else if (model.legacyPredictionService.contains("24")) {
-                    runtimeProvider.jaqpotpyInferenceLegacy24
-                } else {
-                    throw JaqpotRuntimeException("unknown runtime with predictionService ${model.legacyPredictionService}")
+
+                ModelType.SKLEARN -> {
+                    runtimeProvider.jaqpotpyInferenceV6Url
                 }
-            } else {
-                throw JaqpotRuntimeException("unknown runtime with predictionService ${model.legacyPredictionService}")
+
+                else -> {
+                    throw JaqpotRuntimeException("Model type is not yet supported")
+                }
             }
+        )
+    }
+
+    private fun legacyResolveRuntime(model: Model): Optional<String> {
+        val legacyPredictionService = model.legacyPredictionService
+
+        if (legacyPredictionService != null) {
+            return Optional.of(resolveRuntimeWithPredictionService(legacyPredictionService, model))
         }
 
-        return when (model.type) {
-            ModelType.R -> {
-                runtimeProvider.jaqpotRUrl
-            }
+        return Optional.empty()
+    }
 
-            ModelType.SKLEARN -> {
-                runtimeProvider.jaqpotpyInferenceV6Url
+    private fun resolveRuntimeWithPredictionService(legacyPredictionService: String, model: Model): String {
+        if (legacyPredictionService.contains("jaqpot-r")) {
+            return if (legacyPredictionService.contains("predict.pbpk")) {
+                "${runtimeProvider.jaqpotRUrl}/ocpu/library/GenericR/R/predict.pbpk/json"
+            } else if (legacyPredictionService.contains("jaqpot.predict.caret")) {
+                "${runtimeProvider.jaqpotRUrl}/ocpu/library/GenericR/R/jaqpot.predict.caret/json"
+            } else {
+                throw JaqpotRuntimeException("unknown runtime with predictionService $legacyPredictionService")
             }
-
-            else -> {
-                throw JaqpotRuntimeException("Model type is not yet supported")
+        } else if (legacyPredictionService.contains("jaqpot-inference.jaqpot")) {
+            return runtimeProvider.jaqpotpyInferenceV6Url
+        } else if (legacyPredictionService.contains("jaqpot-python")) {
+            return if (legacyPredictionService.contains("22")) {
+                runtimeProvider.jaqpotpyInferenceLegacy22
+            } else if (legacyPredictionService.contains("23")) {
+                runtimeProvider.jaqpotpyInferenceLegacy23
+            } else if (legacyPredictionService.contains("24")) {
+                runtimeProvider.jaqpotpyInferenceLegacy24
+            } else {
+                throw JaqpotRuntimeException("unknown runtime with predictionService $legacyPredictionService")
             }
+        } else {
+            throw JaqpotRuntimeException("unknown runtime with predictionService $legacyPredictionService")
         }
     }
 }
