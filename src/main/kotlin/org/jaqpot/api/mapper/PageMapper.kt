25cd85630b709ed7d8e807e9708a9142aa35cbca
Alex Arvanitidis <alex.arvanitidis.ntua.gr@gmail.com>
2024-06-19 16:11:53 +0300

    feat(JAQPOT-145): get shared models from organizations (#38)

--- src/main/kotlin/org/jaqpot/api/mapper/PageMapper.kt
+++ src/main/kotlin/org/jaqpot/api/mapper/PageMapper.kt
@@ -5,7 +5,7 @@ import org.jaqpot.api.model.GetModels200ResponseDto
 import org.jaqpot.api.model.UserDto
 import org.springframework.data.domain.Page
 
-fun Page<Model>.toGetModels200ResponseDto(creatorDto: UserDto): GetModels200ResponseDto {
+fun Page<Model>.toGetModels200ResponseDto(creatorDto: UserDto?): GetModels200ResponseDto {
     return GetModels200ResponseDto(
         this.content.map { it.toDto(creatorDto) },
         this.totalElements.toInt(),
--- src/main/kotlin/org/jaqpot/api/repository/ModelRepository.kt
+++ src/main/kotlin/org/jaqpot/api/repository/ModelRepository.kt
@@ -3,9 +3,20 @@ package org.jaqpot.api.repository
 import org.jaqpot.api.entity.Model
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 
 interface ModelRepository : PagingAndSortingRepository<Model, Long>, CrudRepository<Model, Long> {
     fun findAllByCreatorId(creatorId: String, pageable: Pageable): Page<Model>
+
+    @Query(
+        """
+        SELECT m FROM Model m
+        JOIN m.organizations o 
+        JOIN o.userIds u 
+        WHERE u = :userId
+        """
+    )
+    fun findAllSharedWithUser(userId: String, pageable: Pageable): Page<Model>
 }
