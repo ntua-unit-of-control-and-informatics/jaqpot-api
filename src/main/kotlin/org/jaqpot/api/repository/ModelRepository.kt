9b8666a65f59fb3a731dc5b6b2512b433aa1752d
alarv <alarvfm@gmail.com>
2024-06-21 17:07:37 +0300

    fix: fix search query

--- src/main/kotlin/org/jaqpot/api/repository/ModelRepository.kt
+++ src/main/kotlin/org/jaqpot/api/repository/ModelRepository.kt
@@ -9,6 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository
 import java.util.*
 
 interface ModelRepository : PagingAndSortingRepository<Model, Long>, CrudRepository<Model, Long> {
+
     fun findAllByCreatorId(creatorId: String, pageable: Pageable): Page<Model>
     fun findOneByLegacyId(legacyId: String): Optional<Model>
 
@@ -24,8 +25,8 @@ interface ModelRepository : PagingAndSortingRepository<Model, Long>, CrudReposit
 
     @Query(
         value = """
-            SELECT *, ts_rank_cd(textsearchable_index_col, query) AS rank 
-            FROM model, to_tsquery('Weighted | (dark & matter)') query
+            SELECT *, ts_rank_cd(textsearchable_index_col, to_tsquery(:query)) AS rank 
+            FROM model, to_tsquery(:query) query
             WHERE textsearchable_index_col @@ query
             ORDER BY rank DESC
             """,
