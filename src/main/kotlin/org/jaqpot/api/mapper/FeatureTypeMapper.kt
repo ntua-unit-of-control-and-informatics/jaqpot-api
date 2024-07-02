27ac9c6713c3ca2c62632aed455cbe4788f956a6
alarv <alarvfm@gmail.com>
2024-07-02 15:20:33 +0300

    feat(JAQPOT-170): feature patch endpoint

--- src/main/kotlin/org/jaqpot/api/mapper/FeatureTypeMapper.kt
+++ src/main/kotlin/org/jaqpot/api/mapper/FeatureTypeMapper.kt
@@ -1,27 +1,27 @@
 package org.jaqpot.api.mapper
 
 import org.jaqpot.api.entity.FeatureType
-import org.jaqpot.api.model.FeatureDto
+import org.jaqpot.api.model.FeatureTypeDto
 
-fun FeatureDto.FeatureType.toEntity(): FeatureType {
+fun FeatureTypeDto.toEntity(): FeatureType {
     return when (this) {
-        FeatureDto.FeatureType.INTEGER -> FeatureType.INTEGER
-        FeatureDto.FeatureType.FLOAT -> FeatureType.FLOAT
-        FeatureDto.FeatureType.CATEGORICAL -> FeatureType.CATEGORICAL
-        FeatureDto.FeatureType.STRING -> FeatureType.STRING
-        FeatureDto.FeatureType.TEXT -> FeatureType.TEXT
-        FeatureDto.FeatureType.SMILES -> FeatureType.SMILES
+        FeatureTypeDto.INTEGER -> FeatureType.INTEGER
+        FeatureTypeDto.FLOAT -> FeatureType.FLOAT
+        FeatureTypeDto.CATEGORICAL -> FeatureType.CATEGORICAL
+        FeatureTypeDto.STRING -> FeatureType.STRING
+        FeatureTypeDto.TEXT -> FeatureType.TEXT
+        FeatureTypeDto.SMILES -> FeatureType.SMILES
     }
 }
 
-fun FeatureType.toDto(): FeatureDto.FeatureType {
+fun FeatureType.toDto(): FeatureTypeDto {
     return when (this) {
-        FeatureType.INTEGER -> FeatureDto.FeatureType.INTEGER
-        FeatureType.FLOAT -> FeatureDto.FeatureType.FLOAT
-        FeatureType.CATEGORICAL -> FeatureDto.FeatureType.CATEGORICAL
-        FeatureType.STRING -> FeatureDto.FeatureType.STRING
-        FeatureType.TEXT -> FeatureDto.FeatureType.TEXT
-        FeatureType.SMILES -> FeatureDto.FeatureType.SMILES
+        FeatureType.INTEGER -> FeatureTypeDto.INTEGER
+        FeatureType.FLOAT -> FeatureTypeDto.FLOAT
+        FeatureType.CATEGORICAL -> FeatureTypeDto.CATEGORICAL
+        FeatureType.STRING -> FeatureTypeDto.STRING
+        FeatureType.TEXT -> FeatureTypeDto.TEXT
+        FeatureType.SMILES -> FeatureTypeDto.SMILES
     }
 }
 
---
+++ src/main/kotlin/org/jaqpot/api/service/model/FeatureService.kt
@@ -0,0 +1,47 @@
+package org.jaqpot.api.service.model
+
+import org.jaqpot.api.FeatureApiDelegate
+import org.jaqpot.api.mapper.toDto
+import org.jaqpot.api.mapper.toEntity
+import org.jaqpot.api.model.FeatureDto
+import org.jaqpot.api.model.FeatureTypeDto
+import org.jaqpot.api.model.PartiallyUpdateModelFeatureRequestDto
+import org.jaqpot.api.repository.FeatureRepository
+import org.jaqpot.api.repository.ModelRepository
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class FeatureService(private val modelRepository: ModelRepository, private val featureRepository: FeatureRepository) :
+    FeatureApiDelegate {
+    @PreAuthorize("@partialModelUpdateAuthorizationLogic.decide(#root, #modelId)")
+    override fun partiallyUpdateModelFeature(
+        modelId: Long,
+        featureId: Long,
+        partiallyUpdateModelFeatureRequestDto: PartiallyUpdateModelFeatureRequestDto
+    ): ResponseEntity<FeatureDto> {
+        modelRepository.findById(modelId).orElseThrow {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Model with id $modelId not found")
+        }
+
+        val feature = featureRepository.findById(featureId).orElseThrow {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Feature with id $featureId not found")
+        }
+
+        partiallyUpdateModelFeatureRequestDto.name.let { feature.name = it }
+        partiallyUpdateModelFeatureRequestDto.description?.let { feature.description = it }
+        partiallyUpdateModelFeatureRequestDto.featureType.let { feature.featureType = it.toEntity() }
+        if (partiallyUpdateModelFeatureRequestDto.featureType != FeatureTypeDto.CATEGORICAL) {
+            feature.possibleValues = null
+        } else {
+            partiallyUpdateModelFeatureRequestDto.possibleValues.let { feature.possibleValues = it }
+        }
+
+        val featureDto = featureRepository.save(feature).toDto()
+
+        return ResponseEntity.ok(featureDto)
+    }
+}
