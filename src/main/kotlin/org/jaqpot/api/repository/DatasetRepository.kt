3dfbb8a29d06857fda0916d317c5043f7f6b2c9a
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-07-15 12:19:32 +0300

    feat(JAQPOT-166): your datasets page

--- src/main/kotlin/org/jaqpot/api/repository/DatasetRepository.kt
+++ src/main/kotlin/org/jaqpot/api/repository/DatasetRepository.kt
@@ -3,6 +3,8 @@ package org.jaqpot.api.repository
 import jakarta.transaction.Transactional
 import org.jaqpot.api.entity.Dataset
 import org.jaqpot.api.entity.DatasetStatus
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
@@ -13,4 +15,5 @@ interface DatasetRepository : CrudRepository<Dataset, Long> {
     @Transactional
     @Query("UPDATE Dataset d SET d.status = :datasetStatus WHERE d.id = :id")
     fun updateStatus(id: Long, datasetStatus: DatasetStatus)
+    fun findAllByUserId(userId: String, pageable: Pageable): Page<Dataset>
 }
--- src/main/kotlin/org/jaqpot/api/service/dataset/DatasetService.kt
+++ src/main/kotlin/org/jaqpot/api/service/dataset/DatasetService.kt
@@ -2,14 +2,18 @@ package org.jaqpot.api.service.dataset
 
 import org.jaqpot.api.DatasetApiDelegate
 import org.jaqpot.api.mapper.toDto
+import org.jaqpot.api.mapper.toGetDatasets200ResponseDto
 import org.jaqpot.api.model.DatasetDto
+import org.jaqpot.api.model.GetDatasets200ResponseDto
 import org.jaqpot.api.repository.DatasetRepository
+import org.jaqpot.api.service.authentication.AuthenticationFacade
+import org.springframework.data.domain.PageRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.stereotype.Service
 
 @Service
-class DatasetService(private val datasetRepository: DatasetRepository) : DatasetApiDelegate {
+class DatasetService(private val datasetRepository: DatasetRepository, private val authenticationFacade: AuthenticationFacade) : DatasetApiDelegate {
 
     @PostAuthorize("@getDatasetAuthorizationLogic.decide(#root)")
     override fun getDatasetById(id: Long): ResponseEntity<DatasetDto> {
@@ -19,5 +23,14 @@ class DatasetService(private val datasetRepository: DatasetRepository) : Dataset
             ResponseEntity.ok(it.toDto())
         }
             .orElse(ResponseEntity.notFound().build())
+    }
+
+    override fun getDatasets(page: kotlin.Int,
+                             size: kotlin.Int): ResponseEntity<GetDatasets200ResponseDto> {
+        val userId = authenticationFacade.userId
+        val pageable = PageRequest.of(page, size)
+        val datasets = datasetRepository.findAllByUserId(userId, pageable)
+
+        return ResponseEntity.ok().body(datasets.toGetDatasets200ResponseDto())
     }
 }
