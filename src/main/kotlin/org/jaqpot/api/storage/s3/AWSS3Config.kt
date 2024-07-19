414d9b93d400597eb17b5fc44789a2ee3fabb333
alarv <alarvfm@gmail.com>
2024-07-19 09:36:30 +0300

    fix: attempt to get credentials from env

--- src/main/kotlin/org/jaqpot/api/storage/s3/AWSS3Config.kt
+++ src/main/kotlin/org/jaqpot/api/storage/s3/AWSS3Config.kt
@@ -4,6 +4,7 @@ import org.jaqpot.api.aws.AWSConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 
@@ -17,6 +18,7 @@ class AWSS3Config(
     @Bean("s3Client")
     fun s3Client(awsConfig: AWSConfig): S3Client {
         return S3Client.builder()
+            .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
             .region(Region.of(awsConfig.region))
             .build()
     }
