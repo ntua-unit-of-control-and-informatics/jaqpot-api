09b80f79de2d4024a8a8e1f6d940c553e3c15a96
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-09-12 16:44:52 +0300

    fix: remove generic classification model task

--- src/main/kotlin/org/jaqpot/api/mapper/ModelTaskMapper.kt
+++ src/main/kotlin/org/jaqpot/api/mapper/ModelTaskMapper.kt
@@ -6,7 +6,6 @@ import org.jaqpot.api.model.ModelTaskDto
 fun ModelTaskDto.toEntity(): ModelTask {
     return when (this) {
         ModelTaskDto.REGRESSION -> ModelTask.REGRESSION
-        ModelTaskDto.CLASSIFICATION -> ModelTask.CLASSIFICATION
         ModelTaskDto.BINARY_CLASSIFICATION -> ModelTask.BINARY_CLASSIFICATION
         ModelTaskDto.MULTICLASS_CLASSIFICATION -> ModelTask.MULTICLASS_CLASSIFICATION
     }
@@ -15,7 +14,6 @@ fun ModelTaskDto.toEntity(): ModelTask {
 fun ModelTask.toDto(): ModelTaskDto {
     return when (this) {
         ModelTask.REGRESSION -> ModelTaskDto.REGRESSION
-        ModelTask.CLASSIFICATION -> ModelTaskDto.CLASSIFICATION
         ModelTask.BINARY_CLASSIFICATION -> ModelTaskDto.BINARY_CLASSIFICATION
         ModelTask.MULTICLASS_CLASSIFICATION -> ModelTaskDto.MULTICLASS_CLASSIFICATION
     }
