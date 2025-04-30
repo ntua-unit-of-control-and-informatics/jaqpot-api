4bcea820b81d37ef0d6d2634bfee50c2b75a0d9c
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2025-04-30 13:01:18 +0300

    fix: fix dataset result type mapper after datasetResultType changes on openapi

--- src/main/kotlin/org/jaqpot/api/mapper/DatasetResultTypeMapper.kt
+++ src/main/kotlin/org/jaqpot/api/mapper/DatasetResultTypeMapper.kt
@@ -2,10 +2,10 @@ package org.jaqpot.api.mapper
 
 import org.jaqpot.api.model.DatasetResultTypeDto
 
-fun Map<String, DatasetResultTypeDto>.toEntity(): Map<String, String> {
-    return this.mapValues { it.value.value } // enum to its string value (e.g., "BASE64")
+fun Map<String, Any>.toEntity(): Map<String, String> {
+    return this.mapValues { DatasetResultTypeDto.forValue(it.value.toString()).value } // enum to its string value (e.g., "BASE64")
 }
 
-fun Map<String, String>.toDto(): Map<String, DatasetResultTypeDto> {
+fun Map<String, String>.toDto(): Map<String, Any> {
     return this.mapValues { DatasetResultTypeDto.forValue(it.value) }
 }
